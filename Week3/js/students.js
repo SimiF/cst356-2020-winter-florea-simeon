78503bf6ef30b1e4a3c930853913b418e6f50007
SimiF <simeon.florea@oit.edu>
2020-01-29 18:05:00 -0800

    Working on JS

---
+++ Week3/js/students.js
@@ -0,0 +1,6 @@
+var students_data = '{"students_data": [{"student_id":2221243, "email_address":"b.morphew@lab.edu"}, {"student_id":2229612, "email_address":"d.shockley@lab.edu"}, {"student_id":2229183, "email_address":"c.hoch@lab.edu"}, {"student_id":2229463, "email_address":"l.pickerel@lab.edu"}, {"student_id":2226291, "email_address":"m.scouten@lab.edu"}]}'
+
+function returnData(data)
+{
+    return JSON.parse(data);
+}
