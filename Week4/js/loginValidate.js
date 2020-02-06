196417b4758ed39bb679925a3bab54ee6d69cc6b
SimiF <simeon.florea@oit.edu>
2020-02-05 18:37:53 -0800

    Lab mostly finished. Some bugs which will be dicussed with Aden

--- Week4/js/loginValidate.js
+++ Week4/js/loginValidate.js
@@ -2,8 +2,8 @@ function validate() {
     document.getElementById('username-error').style.visibility = 'hidden'
     document.getElementById('password-error').style.visibility = 'hidden'
 
-    let username = document.getElementById('username').value
-    let password = document.getElementById('password').value
+    var username = document.getElementById('username').value
+    var password = document.getElementById('password').value
 
     if (username == '') {
         document.getElementById('username-error').style.visibility = 'visible'
